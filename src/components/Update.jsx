ad8eeba1764e80ee3472606633540d4db0929a46
mehedi-h <mehedihs2015@gmail.com>
2023-10-22 14:27:30 +0600

    modified

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -4,12 +4,29 @@ import { useLoaderData } from "react-router-dom";
 const Update = () => {
 
     const lodedData = useLoaderData()
+    // console.log(lodedData._id);
     const handleUpdate = e => {
         e.preventDefault();
         const form = e.target;
         const name = form.name.value;
         const email = form.email.value;
-        console.log(name, email);
+        const updatedUser = { name, email}
+        console.log(updatedUser);
+
+        fetch(`http://localhost:5000/users/${lodedData._id}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(updatedUser)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if (data.modifiedCount > 0) {
+                alert('User info updated successfully.')
+            }
+        })
     }
 
     return (
